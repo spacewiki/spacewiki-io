0ca585b72a819529b5a0d32564472f41fe44f6dd
Torrie Fischer <tdfischer@hackerbots.net>
2016-12-16 19:25:14 -0800

    init: configure SMTP error mails

--- spacewiki_io/__init__.py
+++ spacewiki_io/__init__.py
@@ -1,4 +1,18 @@
 """Code that runs spacewiki.io"""
 import dispatcher, app
+import logging
+from logging.handlers import SMTPHandler
+import settings as SETTINGS
+
+logging.basicConfig(level=logging.WARNING)
+admin_emails = getattr(SETTINGS, "ADMIN_EMAILS", None)
+if admin_emails is not None:
+    mail_handler = SMTPHandler('127.0.0.1',
+                               'server-error@spacewiki.io',
+                               admin_emails, "SpaceWiki Crash!")
+    mail_handler.setLevel(logging.ERROR)
+    logging.getLogger().addHandler(mail_handler)
+else:
+    logging.warn("No error reporting email is set!")
 
 application = dispatcher.SubdomainDispatcher('spacewiki.io')
