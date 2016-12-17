fe724a91010392abc6826a79c16c903c4fb0a352
Torrie Fischer <tdfischer@hackerbots.net>
2016-12-16 18:36:13 -0800

    ansibilize spacewiki.io

---
+++ ansible/templates/local_hosted_settings.py
@@ -0,0 +1,8 @@
+STRIPE_SECRET_KEY = '{{stripe_secret_key}}'
+STRIPE_KEY = '{{stripe_key}}'
+SLACK_KEY = '{{slack_key}}'
+SLACK_SECRET = '{{slack_secret}}'
+SLACK_VERIFICATION_TOKEN = '{{slack_verification_token}}'
+SECRET_SESSION_KEY = '{{secret_session_key}}'
+PREFERRED_URL_SCHEME = 'https'
+SESSION_COOKIE_DOMAIN = '.spacewiki.io'
