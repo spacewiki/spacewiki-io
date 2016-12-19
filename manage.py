24af1530e8941d78668f0d03ae0432fde123426d
Torrie Fischer <tdfischer@hackerbots.net>
2016-12-18 16:30:50 -0800

    manage: have runserver also run the dispatcher

--- manage.py
+++ manage.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
-from flask.ext.script import Manager, Shell, Server
-from spacewiki_io import app, model
+from flask_script import Manager, Shell, Server
+from spacewiki_io import app, model, dispatcher
 import logging
 import colorlog
+import os
+import spacewiki_io.settings as SETTINGS
 
 APP = app.create_app()
 MANAGER = Manager(APP)
 MANAGER.add_command('shell', Shell())
-MANAGER.add_command('runserver', Server())
+
+@MANAGER.command
+def runserver():
+    from gevent.wsgi import WSGIServer
+    app = dispatcher.SubdomainDispatcher(SETTINGS.IO_DOMAIN)
+    serv = WSGIServer(('', int(os.environ.get('PORT', 5000))), app, log=logging.getLogger("http"))
+    serv.serve_forever()
 
 @MANAGER.command
 def reset():
