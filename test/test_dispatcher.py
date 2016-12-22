8ddd11fe56ab9210ee35bfb04e15aaae833a75c7
Torrie Fischer <tdfischer@hackerbots.net>
2016-12-22 01:34:06 -0800

    test: update tests to support new dispatcher context, and confirm slack token is passed through

--- test/test_dispatcher.py
+++ test/test_dispatcher.py
@@ -1,3 +1,4 @@
+from flask_testing import TestCase
 import unittest
 import hypothesis
 import hypothesis.strategies as st
@@ -11,14 +12,6 @@ ConfigTemplate = st.dictionaries(st.text(string.ascii_uppercase, min_size=1),
         st.text(string.ascii_uppercase))
 
 class SubspaceTestCase(unittest.TestCase):
-    def setUp(self):
-        self._app = app.create_app(False)
-        self._app.config['ADMIN_DB_URL'] = 'sqlite:///:memory:'
-        self._app.config['SECRET_KEY'] = 'foo'
-        self._app.config['SUBSPACE_SECRET_KEY'] = 'foo'
-        with self._app.app_context():
-            model.ADMIN_DATABASE.create_tables([model.Space])
-
     @hypothesis.given(TeamID, ConfigTemplate, ConfigTemplate, ConfigTemplate)
     def test_inheritance(self, team_id, template, subspaceTemplate, siteConfig):
         merged_template = {}
@@ -29,20 +22,19 @@ class SubspaceTestCase(unittest.TestCase):
         conf = dispatcher.SubspaceConfig(space, merged_template)
         conf.from_mapping(**siteConfig)
 
-        with self._app.app_context():
-            for k, v in template.iteritems():
-                if k in subspaceTemplate or k in siteConfig:
-                    continue
-                with self.assertRaises(KeyError):
-                    conf[k]
+        for k, v in template.iteritems():
+            if k in subspaceTemplate or k in siteConfig:
+                continue
+            with self.assertRaises(KeyError):
+                conf[k]
 
-            for k, v in subspaceTemplate.iteritems():
-                if k in siteConfig:
-                    continue
-                self.assertEqual(conf[k], subspaceTemplate[k])
+        for k, v in subspaceTemplate.iteritems():
+            if k in siteConfig:
+                continue
+            self.assertEqual(conf[k], subspaceTemplate[k])
 
-            for k, v in siteConfig.iteritems():
-                self.assertEqual(conf[k], siteConfig[k])
+        for k, v in siteConfig.iteritems():
+            self.assertEqual(conf[k], siteConfig[k])
         
     @hypothesis.given(TeamID, TeamDomain)
     def test_replacements(self, team_id, domain):
@@ -50,52 +42,46 @@ class SubspaceTestCase(unittest.TestCase):
         template = {'SUBSPACE_DOMAIN': '%(domain)s', 'SUBSPACE_ID':
         '%(slack_id)s'}
         conf = dispatcher.SubspaceConfig(space, template)
-        with self._app.app_context():
-            self.assertEqual(conf['DOMAIN'], domain)
-            self.assertEqual(conf['ID'], team_id)
+        self.assertEqual(conf['DOMAIN'], domain)
+        self.assertEqual(conf['ID'], team_id)
 
 class DispatcherTestCase(unittest.TestCase):
     def setUp(self):
-        self._app = app.create_app(False)
+        self.app = app.create_app(False)
         tmp_dir = tempfile.mkdtemp()
-        self._app.config['ADMIN_DB_URL'] = 'sqlite:///%s/admin.sqlite3'%(tmp_dir)
-        self._app.config['SUBSPACE_DATABASE_URL'] = 'sqlite:///%s/'%(tmp_dir) + '%(slack_id)s.sqlite3'
-        self._app.config['SECRET_KEY'] = 'foo'
-        self._app.config['SUBSPACE_SECRET_KEY'] = 'foo'
-        with self._app.app_context():
+        self.app.config['ADMIN_DB_URL'] = 'sqlite:///%s/admin.sqlite3'%(tmp_dir)
+        self.app.config['SUBSPACE_DATABASE_URL'] = 'sqlite:///%s/space.sqlite3'%(tmp_dir)
+        with self.app.app_context():
+            model.get_db()
             model.ADMIN_DATABASE.create_tables([model.Space], True)
-        self.dispatcher = dispatcher.Dispatcher(self._app)
-
-    @hypothesis.given(TeamID, TeamDomain)
-    def test_app_config(self, team_id, domain):
-        space = model.Space(slack_team_id=team_id, domain=domain)
-        with self.dispatcher.dispatch_context():
-            with self._app.app_context():
-                space_app = self.dispatcher.get_wiki_app(space, False)
+        self.dispatcher = dispatcher.Dispatcher(self.app)
 
     @hypothesis.given(TeamID, TeamDomain, TeamDomain)
     def test_subdomain(self, team_id, domain, io_domain):
-        self._app.config['IO_DOMAIN'] = io_domain
+        self.app.config['IO_DOMAIN'] = io_domain
         with self.dispatcher.dispatch_context():
-            with self._app.app_context():
+            with self.app.app_context():
                 space = model.Space.create(slack_team_id=team_id, domain=domain)
                 space_app = self.dispatcher.get_wiki_app(space, False)
                 subdomain_app = self.dispatcher.get_application(space.domain+'.'+io_domain)
                 self.assertEqual(space_app, subdomain_app)
-
-                subdomain_client = subdomain_app.test_client()
-                resp = subdomain_client.get('/')
-                self.assertEqual(resp.status_code, 403)
+                self.app.test_client().get('/')
 
     @hypothesis.given(TeamDomain, TeamDomain)
     def test_dead_space(self, domain, io_domain):
-        self._app.config['IO_DOMAIN'] = io_domain
+        self.app.config['IO_DOMAIN'] = io_domain
         with self.dispatcher.dispatch_context():
-            with self._app.app_context():
+            with self.app.app_context():
                 subdomain_app = self.dispatcher.get_application(domain+'.'+io_domain)
                 self.assertNotEqual(subdomain_app, self.dispatcher.default_app)
                 self.assertEqual(subdomain_app, self.dispatcher.deadspace_app)
+                self.app.test_client().get('/')
 
-                subdomain_client = subdomain_app.test_client()
-                self.assertEqual(subdomain_client.get('/').status_code, 404)
-                self.assertEqual(subdomain_client.get('/static/cache/spacewiki-io-app.css').status_code, 200)
+    @hypothesis.given(TeamDomain)
+    def test_default_app(self, io_domain):
+        self.app.config['IO_DOMAIN'] = io_domain
+        with self.dispatcher.dispatch_context():
+            with self.app.app_context():
+                subdomain_app = self.dispatcher.get_application(io_domain)
+                self.assertEqual(subdomain_app, self.dispatcher.default_app)
+                self.app.test_client().get('/')
--- test/test_wrapper.py
+++ test/test_wrapper.py
@@ -2,28 +2,44 @@ from flask import Flask, request, session
 import hypothesis
 import hypothesis.strategies as st
 import unittest
-from spacewiki_io import io_wrapper, io_common, dispatcher
-from spacewiki import app, model
+from spacewiki_io import io_wrapper, io_common, dispatcher, app
+from flask_testing import TestCase
+import spacewiki.app
+import spacewiki.model
 from flask_login import current_user
 import tempfile
 
-class WrapperTestCase(unittest.TestCase):
-    def setUp(self):
-        self.app = app.create_app(False)
+class WrapperTestCase(TestCase):
+    def create_app(self):
+        ret = spacewiki.app.create_app(False)
         tmp_dir = tempfile.mkdtemp()
-        self.app.config['DATABASE_URL'] = 'sqlite:///%s/db.sqlite3'%(tmp_dir)
-        with self.app.app_context():
-            model.syncdb()
-        self.app.secret_key = 'foo'
-        self.app.register_blueprint(io_wrapper.BLUEPRINT)
-        self.app.register_blueprint(io_common.BLUEPRINT)
+        ret.config['DATABASE_URL'] = 'sqlite:///%s/space.sqlite3'%(tmp_dir)
+        ret.config['SECRET_KEY'] = 'foo'
+        ret.register_blueprint(io_wrapper.BLUEPRINT)
+        ret.register_blueprint(io_common.BLUEPRINT)
+        with ret.app_context():
+            spacewiki.model.syncdb()
+        return ret
+
+    def setUp(self):
         self.client = self.app.test_client()
-        self.dispatcher = dispatcher.Dispatcher(self.app)
+        dispatch_app = app.create_app(False)
+        dispatch_app.config['IO_DOMAIN'] = 'localhost'
+        dispatch_app.config['IO_SCHEME'] = 'http'
+        dispatch_app.config['SECRET_KEY'] = 'foo'
+        self.dispatcher = dispatcher.Dispatcher(dispatch_app)
+
+    def test_template_vars(self):
+        with self.dispatcher.dispatch_context():
+            with self.app.test_client() as client:
+                client.get('/')
+                self.assertEqual(self.get_context_variable('dispatcher_settings'),
+                        self.dispatcher.default_app.config)
 
     @hypothesis.given(st.text(min_size=5))
     def test_cross_domain_login(self, auth_id):
         with self.app.app_context():
-            login_user,_ = model.Identity.get_or_create(auth_id=auth_id,
+            login_user,_ = spacewiki.model.Identity.get_or_create(auth_id=auth_id,
                     auth_type='slack',
                     display_name='',
                     handle='')
@@ -45,7 +61,8 @@ class WrapperTestCase(unittest.TestCase):
                 with client.session_transaction() as sess:
                     sess['_spacewikiio_auth_id'] = auth_id
                 self.assertEqual(client.get('/').status_code, 403)
-                self.assertEqual(client.get('/static/cache/spacewiki-io-app.css').status_code, 200)
+                self.assertNotEqual(client.get('/static/cache/spacewiki-io-app.css').status_code,
+                        403)
                 self.assertFalse(current_user.is_authenticated)
                 self.assertTrue(current_user.is_anonymous)
                 self.assertTrue('_spacewikiio_auth_id' not in session)
@@ -54,7 +71,8 @@ class WrapperTestCase(unittest.TestCase):
         with self.dispatcher.dispatch_context():
             with self.app.test_client() as client:
                 self.assertEqual(client.get('/').status_code, 403)
-                self.assertEqual(client.get('/static/cache/spacewiki-io-app.css').status_code, 200)
+                self.assertNotEqual(client.get('/static/cache/spacewiki-io-app.css').status_code,
+                        403)
                 self.assertNotEqual(current_user, None)
                 self.assertFalse(current_user.is_authenticated)
                 self.assertTrue(current_user.is_anonymous)
